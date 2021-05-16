9f202c1ef1007b60038e69395ff2b61993079b48
hassounekkk <79729458+hassounekkk@users.noreply.github.com>
2021-05-16 03:26:48 +0100

    ColorGame

--- Assets/ColorsController.cs
+++ Assets/ColorsController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class ColorsController : MonoBehaviour
 {
+    public Color curColer;
+    public GameObject chosenColor;
+    SpriteRenderer SpriteRenderers;
+    public Color[] colors;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpriteRenderers = chosenColor.GetComponent<SpriteRenderer>();
+        SpriteRenderers.color = curColer;
+    }
+
+     public void ChooseColer(int nbr)
+    {
+        curColer = colors[nbr];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SpriteRenderers.color = curColer;
     }
 }
